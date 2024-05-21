0f4b9ea89721ce5b4641f104f5e8ba4df529e472
CoffeeSi <ewgenik02032006@gmail.com>
2024-05-21 22:04:13 +0500

    add aircraft model and add rotation of the model

--- static/main.js
+++ static/main.js
@@ -1,34 +1,75 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 
     window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+const ambientLight = new THREE.AmbientLight( 0xffffff );
+scene.add( ambientLight );
+
+const pointLight = new THREE.PointLight( 0xffffff, 15 );
+camera.add( pointLight );
+
+let object;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight, false);
 document.body.appendChild( renderer.domElement );
 
-const texture = new 
-THREE.TextureLoader().load('image/fuselage_albd.png');
+function load_model() {
+    object.traverse( function (child) {
+        if ( child.isMesh ) child.material.map = texture;
+    });
+    scene.add(object);
+}
 
-const material = new THREE.MeshBasicMaterial( {map:texture} );
+const manager = new THREE.LoadingManager( load_model );
 
-const loader = new GLTFLoader();
-loader.load( 'models/cessna-cj4-06.glb', 
-    function ( gltf ) {
-        scene.add( gltf.scene );
-    }, undefined, function ( error ) {
-        console.log( error );
-});
+const texture_loader = new THREE.TextureLoader( manager );
+const texture = texture_loader.load('image/fuselage_albd.png', animate);
+texture.colorSpace = THREE.SRGBColorSpace;
+
+function onProgress( xhr ) {
+
+    if ( xhr.lengthComputable ) {
+
+        const percentComplete = xhr.loaded / xhr.total * 100;
+        console.log( 'model ' + percentComplete.toFixed( 2 ) + '% downloaded' );
+
+    }
+
+}
+
+function onError() {}
+
+const loader = new OBJLoader( manager );
+loader.load('models/cessna-cj4-06.obj', function (obj) {
+    object = obj;
+}, onProgress, onError);
 
 renderer.setClearColor( 0xffffff );
-camera.position.z = 10;
-camera.position.y = 5;
-camera.position.x = 6;
+camera.position.set(6,5,9);
 
 camera.rotateY(.7);
 camera.rotateX(-.3);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 10;
+controls.maxDistance = 15;
+controls.addEventListener('change', animate);
+
+window.addEventListener('resize',onWindowResize);
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 function animate() {
     requestAnimationFrame( animate );
